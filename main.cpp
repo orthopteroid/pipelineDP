03926fe08d94904223e1a5e8ecbac93e41a6c5f3
john howard <orthopteroid@gmail.com>
2023-07-14 12:35:18 -0700

    variable-name tidy, symbols for numerical consts

--- main.cpp
+++ main.cpp
@@ -12,27 +12,25 @@
 using Node = uint16_t;
 using Edge = std::pair<Node, Node>;
 
-struct EdgeCost { float fixed, parameterized, total; };
-using EdgeMap = std::map<Edge, EdgeCost>;
+struct EdgeInfo { float edge_cost; };
+using EdgeMap = std::map<Edge, EdgeInfo>;
 using DAG = std::vector< std::deque<Node> >;
 
 struct XYZ { float x, y, z; };
-struct NodeAndCost { Node node; float cost; };
+struct NodeAndCost { Node node; float path_cost; };
 
 const NodeAndCost nacMAX = {std::numeric_limits<uint16_t>::max(), std::numeric_limits<float>::max()};
 const NodeAndCost nacZERO = {std::numeric_limits<uint16_t>::max(), 0};
 
-// determine cost of an edge from # of river crossings and % of different terrain type over a length in feet
-constexpr EdgeCost FillEdgeCost(
-        const int riv_c,
-        const float wat_p, const float swa_p, const float roc_p, const float soi_p, const float str_p, const float btr_p
-)
-{
-    return {riv_c * 10000.0f, wat_p * 1.5f + swa_p * .8f + roc_p * 2.5f + soi_p * .2f + str_p * 0.0f + btr_p * .55f, 0.0f };
-}
+enum : int {NoTrees = 0, SmallTrees, LargeTrees};
+enum : int {Water = 1, Swamp, Rock, Soil};
 
-template<class T>
-T deque_front_pop(std::deque<T>& deque) { T t = deque.front(); deque.pop_front(); return t; }
+EdgeInfo edge_info(const int rc, const int lt, const int tt, const float length)
+{
+    static std::array<float, 3> tree_costs = { 0, 0, 0.55, }; // none, small, large
+    static std::array<float, 5> land_costs = { 0, 1.5, 0.8, 2.5, 0.2, }; // none, water, swamp, rock, soil
+    return {rc * 10000 + length * (land_costs[lt] + tree_costs[tt]) };
+}
 
 /////////
 
@@ -54,81 +52,86 @@ DAG fwd_linkage =
 
 EdgeMap edge_cost =
 {
-    { {0,1}, FillEdgeCost(0, 0, 0, 0, 1., 0, 0) },
-    { {0,2}, FillEdgeCost(0, 0, 0, 0, 1., 0, 0) },
-    { {1,3}, FillEdgeCost(0, 0, 0, 0, 1., 0, 0) },
-    { {2,3}, FillEdgeCost(0, 0, 0, 0, 1., 0, 0) },
+    { {0,1}, edge_info(0, Soil, SmallTrees, 4500) },
+    { {0,2}, edge_info(0, Soil, SmallTrees, 6000) },
+    { {1,3}, edge_info(0, Soil, SmallTrees, 5800) },
+    { {2,3}, edge_info(0, Soil, SmallTrees, 3500) },
 };
 
 int main()
 {
-    std::deque<Node> child_nodes;
+    std::deque<Node> visit_stack;
+    std::vector<bool> fwd_visit_marking(fwd_linkage.size());
 
-    auto NodeDist =[&](const Node a, const Node b) -> float
+    auto visit_stack_pop = [&]()
+    { Node t = visit_stack.front(); visit_stack.pop_front(); return t; };
+
+    auto visit_stack_unique_visitation = [&] (Node n)
     {
-        float dx = node_pos[a].x - node_pos[b].x;
-        float dy = node_pos[a].y - node_pos[b].y;
-        return sqrt( dx * dx + dy * dy );
+        if (!fwd_visit_marking[n])
+        {
+            fwd_visit_marking[n] = true;
+            visit_stack.push_back(n);
+        }
     };
 
-    DAG back_linkage;
-    back_linkage.resize(fwd_linkage.size() );
-
-    // init individual edge costs
-    for(auto it = edge_cost.begin(); it != edge_cost.end(); ++it)
-        it->second.total = it->second.fixed + it->second.parameterized * NodeDist( it->first.first, it->first.second );
+    DAG bwd_linkage;
+    bwd_linkage.resize(fwd_linkage.size() );
 
     std::cout << "Linkages and costs:\n";
-    child_nodes = {0};
-    while (!child_nodes.empty()) {
-        Node f = deque_front_pop(child_nodes);
+    visit_stack = {0};
+    std::fill(fwd_visit_marking.begin(), fwd_visit_marking.end(), false);
+    while (!visit_stack.empty())
+    {
+        Node f = visit_stack_pop();
         std::cout << f << ": ";
-        std::for_each(fwd_linkage[f].begin(), fwd_linkage[f].end(), [&](Node t) {
-            std::cout << t << " " << edge_cost[{f, t}].total << " ";
-            if(child_nodes.front() != t)
-                child_nodes.push_back(t);
+        std::for_each(fwd_linkage[f].begin(), fwd_linkage[f].end(), [&](Node t)
+        {
+            std::cout << t << " " << edge_cost[{f, t}].edge_cost << " ";
+            visit_stack_unique_visitation(t);
         });
         std::cout << "\n";
     }
 
-    // init local minima vector
-    std::vector<NodeAndCost> fwd_local_min;
-    fwd_local_min.resize(fwd_linkage.size() );
-    std::fill(fwd_local_min.begin(), fwd_local_min.end(), nacMAX );
-    fwd_local_min[0] = nacZERO;
+    std::vector<NodeAndCost> fwd_local_min(fwd_linkage.size(), nacMAX );
 
     // accumulate minimum forward pass
     // start at node 0
-    child_nodes = {0};
-    while (!child_nodes.empty()) {
-        Node f = deque_front_pop(child_nodes);
-        std::for_each(fwd_linkage[f].begin(), fwd_linkage[f].end(), [&](Node t) {
-            auto local_cost = fwd_local_min[f].cost + edge_cost[{f, t}].total;
-            if (local_cost < fwd_local_min[t].cost)
+    fwd_local_min[0] = nacZERO;
+    visit_stack = {0};
+    std::fill(fwd_visit_marking.begin(), fwd_visit_marking.end(), false);
+    while (!visit_stack.empty())
+    {
+        Node f = visit_stack_pop();
+        std::for_each(fwd_linkage[f].begin(), fwd_linkage[f].end(), [&](Node t)
+        {
+            auto local_cost = fwd_local_min[f].path_cost + edge_cost[{f, t}].edge_cost;
+            if (local_cost < fwd_local_min[t].path_cost)
                 fwd_local_min[t] = {f, local_cost};
-            if(child_nodes.front() != t)
-                child_nodes.push_back(t);
-            back_linkage[t].push_back(f);
+            visit_stack_unique_visitation(t);
+            bwd_linkage[t].push_back(f);
         });
     }
 
     // select minimum backward pass
-    // start at node 'back_linkage.size() -1', end at node 0
-    std::deque<NodeAndCost> back_global_min;
-    Node t = back_linkage.size() -1;
-    back_global_min.push_front( {t, fwd_local_min[t].cost} );
-    while (t != 0) {
+    // start at node 'bwd_linkage.size() -1', end at node 0
+    std::deque<NodeAndCost> bwd_global_min;
+    Node t = bwd_linkage.size() - 1;
+    bwd_global_min.push_front({t, fwd_local_min[t].path_cost} );
+    while (t != 0)
+    {
         NodeAndCost min_route_cost = nacMAX;
-        std::for_each(back_linkage[t].begin(), back_linkage[t].end(), [&](Node f) {
-            if (fwd_local_min[f].cost < min_route_cost.cost)
-                min_route_cost = {f, fwd_local_min[f].cost};
+        std::for_each(bwd_linkage[t].begin(), bwd_linkage[t].end(), [&](Node f)
+        {
+            if (fwd_local_min[f].path_cost < min_route_cost.path_cost)
+                min_route_cost = {f, fwd_local_min[f].path_cost};
         });
-        back_global_min.push_front(min_route_cost);
+        bwd_global_min.push_front(min_route_cost);
         t = min_route_cost.node;
     }
 
-    std::cout << "Optimal route and cumulative cost:\n";
-    std::for_each(back_global_min.begin(), back_global_min.end(), [&](NodeAndCost& nc)
-        { std::cout << nc.node << ' ' << nc.cost << "\n"; });
+    std::cout << "Optimal route and cumulative path_cost:\n";
+    std::for_each(bwd_global_min.begin(), bwd_global_min.end(), [&](NodeAndCost& nc)
+    { std::cout << nc.node << ' ' << nc.path_cost << "\n"; });
     return 0;
 }
